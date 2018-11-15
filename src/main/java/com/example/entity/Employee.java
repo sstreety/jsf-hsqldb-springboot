bf3a4723de1f53bcfc59ec91cedf2f44abfd9919
susanne streety <s.streety@appliedmemeticsllc.com>
2018-11-15 17:04:01 -0600

    initial commit

---
+++ src/main/java/com/example/entity/Employee.java
@@ -0,0 +1,22 @@
+package com.example.entity;
+
+public class Employee {
+    private String employeeId;
+    private String employeeName;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+}
---
+++ src/main/java/com/example/entity/Product.java
@@ -0,0 +1,32 @@
+package com.example.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private BigDecimal price;
+
+    protected Product() {
+    }
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+}
